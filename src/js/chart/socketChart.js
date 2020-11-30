f1667470235b9a78334a542780cd238baf91a04d
Samarit <kislyakaleksey@gmail.com>
2020-11-30 22:34:05 +0300

    socketChart in progress

---
+++ src/js/chart/socketChart.js
@@ -0,0 +1,32 @@
+import { chartCandle } from "../../chart.models.js/chart";
+import chartState from "../../chart.models.js/chartState";
+
+export default function socketChart() {
+    let opened = false
+    let socket = new WebSocket(`wss://stream.binance.com:9443/ws/${chartState.symbol.toLowerCase()}@kline_${chartState.timeframe}`)
+
+    socket.onmessage = _onMsg
+
+    socket.onclose = () => console.log('socketChart closed')
+
+    return {
+        close() {
+            opened && socket.close()
+        },
+        open() {
+            socket = new WebSocket(`wss://stream.binance.com:9443/ws/${chartState.symbol.toLowerCase()}@kline_${chartState.timeframe}`)
+            opened = true
+            socket.onmessage = _onMsg
+
+            socket.onclose = () => console.log('socketChart closed')
+        }
+    }
+}
+
+function _onMsg(message) {
+    const data = JSON.parse(message.data)
+    const datapoints = chartCandle.charts[0].options.data[0].dataPoints
+    datapoints[datapoints.length - 1].y[3] = Number(data.k.c)
+    console.log('socket')
+    chartCandle.render()
+}
--- src/js/chart/updateChart.js
+++ src/js/chart/updateChart.js
@@ -1,14 +1,21 @@
 import { chartCandle, pushChartDatapoints} from '../../chart.models.js/chart'
 import chartState from '../../chart.models.js/chartState';
 import getChartData from './getChartData';
+import socketChart from './socketChart'
+
+const socket = socketChart()
 
 export default async function updateChart() {
     try {
+        socket.close()
+
         const data = await getChartData()
         pushChartDatapoints(data)
         
         chartCandle.options.charts[0].title.text = chartState.symbol
         chartCandle.render()
+
+        socket.open()
         
         console.log(chartCandle)
     } catch (error) {
