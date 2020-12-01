ed7774deaeb0549ec63bd8bd6dce1ff2b60c8a16
Samarit <kislyakaleksey@gmail.com>
2020-12-02 01:35:00 +0300

    loader added

--- src/js/chart/socketChart.js
+++ src/js/chart/socketChart.js
@@ -18,7 +18,12 @@ export default function socketChart() {
 function _onMsg(message) {
     const data = JSON.parse(message.data)
     const datapoints = chartCandle.charts[0].options.data[0].dataPoints
-    datapoints[datapoints.length - 1].y[3] = Number(data.k.c)
+    
+    //Changing specific datapoints in last chart's kline 
+    datapoints[datapoints.length - 1].y[0] = Number(data.k.o) // open
+    datapoints[datapoints.length - 1].y[1] = Number(data.k.h) // high
+    datapoints[datapoints.length - 1].y[2] = Number(data.k.l) // low
+    datapoints[datapoints.length - 1].y[3] = Number(data.k.c) // close
     console.log('socket')
     chartCandle.render()
 }
--- src/js/chart/updateChart.js
+++ src/js/chart/updateChart.js
@@ -4,14 +4,18 @@ import getChartData from './getChartData';
 import socketChart from './socketChart'
 
 const socket = socketChart()
+const loader = document.getElementById('loader')
 
 export default async function updateChart() {
     try {
+        loader.style.display = 'flex'
         const data = await getChartData()
         pushChartDatapoints(data)
         
         chartCandle.options.charts[0].title.text = chartState.symbol
         chartCandle.render()
+
+        loader.style.display = 'none'
 
         socket.open()
         
