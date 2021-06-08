29061ec599bd6b9e453429d8d50ef0c759438f61
Samarit <kislyakaleksey@gmail.com>
2021-06-09 00:21:18 +0300

    deleted cross-env in build

--- app.js
+++ app.js
@@ -19,8 +19,5 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     })
 }
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+
 app.use('/api/binance', require('./routes/binance.routes'))
