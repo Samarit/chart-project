5189d4d7ae5db52de6f82c57c1f5b9c080f31b1f
Samarit <kislyakaleksey@gmail.com>
2021-06-08 23:45:34 +0300

    node_env del

--- app.js
+++ app.js
@@ -19,5 +19,8 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     })
 }
-
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 app.use('/api/binance', require('./routes/binance.routes'))
